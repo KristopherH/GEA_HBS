4924d7fef182b2d18783c9a7f062e9deccd43016
k9-hill <Kristopher2.Hill@live.uwe.ac.uk>
2017-02-08 15:14:15 +0000

    Added input manager.cpp with empty functions and some more stuff to game object.h

---
+++ ScarleCodeBase-master/DXTK_Wrapper/Input_Manager.cpp
@@ -0,0 +1,78 @@
+#include "Input_Manager.h"
+
+bool InputManager::getMouseRightDown()
+{
+	return false;
+}
+
+
+
+bool InputManager::getMouseRightUp()
+{
+	return false;
+}
+
+
+
+bool InputManager::getMouseRightHeld()
+{
+	return false;
+}
+
+
+
+bool InputManager::getMouseLeftDown()
+{
+	return false;
+}
+
+
+
+bool InputManager::getMouseLeftUp()
+{
+	return false;
+}
+
+
+
+bool InputManager::getMouseLeftHeld()
+{
+	return false;
+}
+
+
+
+bool InputManager::getMouseMiddleDown()
+{
+	return false;
+}
+
+
+
+bool InputManager::getMouseMiddleUp()
+{
+	return false;
+}
+
+
+
+bool InputManager::getMouseMiddleHeld()
+{
+	return false;
+}
+
+
+
+bool InputManager::getKeyDown(unsigned int key)
+{
+	return false;
+}
+
+
+
+bool InputManager::gamePadButtonDown(unsigned int button)
+{
+	return false;
+}
+
+
--- ScarleCodeBase-master/Main_Engine/GameObject.h
+++ ScarleCodeBase-master/Main_Engine/GameObject.h
@@ -1,8 +1,24 @@
 #pragma once
 
+struct Vec2 { float x; float y; }; //DELETE THIS LINE WHEN THE CUSTOMMATH.H IS DONE
+
 class GameObject
 {
 public:
 	GameObject();
 	~GameObject();
+	
+	Vec2 getPosition();
+	Vec2 getSize();
+	float getOpacity();
+
+	void setPosition(Vec2 _position);
+	void setSize(Vec2 _size);
+	void setOpacity(float _opacity);
+
+private:
+	Vec2 position;
+	Vec2 size;
+	float opacity = 0.0f;
+
 };
