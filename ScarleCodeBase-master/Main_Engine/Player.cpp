1faef4a6eccffed2a9f59c694b673f2d251be6df
Elliottphillips443 <elliottphillips443@yahoo.com>
2017-04-18 14:03:05 +0100

    SOUND

--- ScarleCodeBase-master/Main_Engine/Player.cpp
+++ ScarleCodeBase-master/Main_Engine/Player.cpp
@@ -1,6 +1,7 @@
 #include "Player.h"
 //C++
 #include <iostream>
+#include <Audio.h>
 
 //DXTK
 
@@ -138,6 +139,8 @@ void Player::OnJump()
 			//position += Vec2(0.0f, jumpStrength);
 			acceleration += Vec2(0.0f, jumpStrength);
 			stoppedJumping = false;
+			PlaySound("..\\Assets\\Sounds\\jump.wav", NULL, SND_ASYNC);
+
 		}
 	
 		//if you keep holding down the mouse button...
@@ -177,18 +180,21 @@ void Player::OnMove(Vec2 _direction)
 			if (go->getTag() == "Slow Platform")
 			{
 				acceleration += _direction * 0.25;
+				PlaySound("..\\Assets\\Sounds\\Walking.wav", NULL, SND_LOOP | SND_ASYNC);
 				moved = true;
 				break;
 			}
 			else if (go->getTag() == "Speed Platform")
 			{
 				acceleration += _direction * 3.0;
+				PlaySound("..\\Assets\\Sounds\\Walking.wav", NULL, SND_LOOP | SND_ASYNC);
 				moved = true;
 				break;
 			}
 			else
 			{
 				acceleration += _direction;
+				PlaySound("..\\Assets\\Sounds\\Walking.wav", NULL, SND_LOOP | SND_ASYNC);
 				moved = true;
 				break;
 			}
@@ -198,6 +204,7 @@ void Player::OnMove(Vec2 _direction)
 	if (!moved)
 	{
 		acceleration += _direction;
+		PlaySound("..\\Assets\\Sounds\\Walking.wav", NULL, SND_LOOP | SND_ASYNC);
 	}
 
 	if (!key_down)
---
+++ ScarleCodeBase-master/Main_Engine/Sound.h
@@ -0,0 +1,16 @@
+#include <map>
+#include <functional>
+#include <memory>
+
+class Sound  
+{
+public:
+
+	Sound();
+	~Sound();
+
+	void Walking();
+	void Jumping();
+	void BackGround();
+
+}; 
