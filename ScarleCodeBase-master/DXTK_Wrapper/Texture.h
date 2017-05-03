6c72ae00cf5c96c68114c2f6692a0aa7392d0128
Gualtiero Vercellotti <Gualtiero2.Vercellotti@live.uwe.ac.uk>
2017-05-03 16:11:14 +0100

    PixelLoader in Texture

--- ScarleCodeBase-master/DXTK_Wrapper/Texture.h
+++ ScarleCodeBase-master/DXTK_Wrapper/Texture.h
@@ -1,6 +1,7 @@
 #pragma once
 //C++
 #include <string>
+#include <vector>
 
 //DXTK
 #include <d3d11_1.h>
@@ -17,8 +18,12 @@ public:
 	Texture(std::string _fileName, Renderer* _renderer);
 
 	Vec2 getSize();
+	void LoadPixelMap();
 	ID3D11ShaderResourceView* getTexture() { return m_pTextureRV; }
 
 protected:
 	ID3D11ShaderResourceView* m_pTextureRV;
+	ID3D11Texture2D* captureTexture;
+
+	std::vector<Vec4> pixelMap;
 };
