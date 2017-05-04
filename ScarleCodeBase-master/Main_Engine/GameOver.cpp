9c2be07f29296b620ac9ffd3fee1cbbeb7d7e717
Daniel Donaldson <Daniel Donaldson>
2017-05-04 04:34:30 +0100

    pause menu fix

--- ScarleCodeBase-master/Main_Engine/GameOver.cpp
+++ ScarleCodeBase-master/Main_Engine/GameOver.cpp
@@ -30,17 +30,17 @@ GameOver::GameOver()
 	go->setPosition(&((GameData::currentCamera->getCameraSize()/2)*-1));
 	go_list.push_back(go);
 	
-	Text* GOtxt = new Text(nullptr, "txt", "NULL", /*"    oh dear! \n */"Game Over");
+	Text* GOtxt = new Text(nullptr, "txt", "NULL", "Game Over");
 	GOtxt->setSize(new Vec2(800, 800));
 	GOtxt->setPosition(new Vec2((GameData::currentCamera->getViewSize().x / 2) - (GOtxt->getSize().x / 2), -70.0f));
-	GOtxt->setColor(Vec4(0.60f, 0.0f, 0.0f, 1.0f));
+	GOtxt->setColor(Vec4(0.50f, 0.0f, 0.0f, 1.0f));
 
 	Text* Scoretxt = new Text(nullptr, "txt", "NULL", "Final score: " + std::to_string(GameData::player->getScore()));
 	Scoretxt->setSize(new Vec2(450, 450));
 	Scoretxt->setPosition(new Vec2((GameData::currentCamera->getViewSize().x / 2) - (Scoretxt->getSize().x / 2), 295.0f));
-	Scoretxt->setColor(Vec4(0.0f, 0.0f, 0.60f, 1.0f));
+	Scoretxt->setColor(Vec4(0.0f, 0.0f, 0.40f, 1.0f));
 
-	Button* menuBtn = new Button(new Sprite("Button", GameData::renderer), "button1", "Button", "Main Menu");
+	Button* menuBtn = new Button(new Sprite("Button3", GameData::renderer), "button1", "Button", "Main Menu");
 	menuBtn->setSize(new Vec2(300.0f, 150.0f));
 	menuBtn->setPosition(new Vec2((GameData::currentCamera->getViewSize().x / 2) - (menuBtn->getSize().x / 2), 700.0f));
 	menuBtn->setOrigin(new Vec2(0.0f, 0.0f));
